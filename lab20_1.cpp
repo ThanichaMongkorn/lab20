e6fa10ac63f3a1870584e7c1176191c0ac6bb030
= <=thanicha_mongkorn@cmu.ac.th>
2023-02-21 17:07:43 +0700

    lab20.2

--- lab20_1.cpp
+++ lab20_1.cpp
@@ -5,8 +5,14 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect R1,Rect R2){
+	double l =min((R2.x+R2.w),(R1.x+R1.w))-max(R1.x,R2.x);
+	double d =min(R1.y,R2.y)-max((R1.y-R1.h),(R2.y-R2.h));
+	if(l<0 or d<0){
+	    return 0;
+	}
 
-
+	return l*d;
+	
 
 }
